f63a67457d9b10b1046840134efbec4fbd98c250
Bowen Ma <bowen.ma@rea-group.com>
2013-09-18 19:17:10 +0800

    add first commit and gradle/travis support

---
+++ test/com/imabowen/HelloTest.java
@@ -0,0 +1,12 @@
+package com.imabowen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloTest {
+    @Test
+    public void testShouldAlwaysBeRight(){
+        assertTrue(true);
+    }
+}
